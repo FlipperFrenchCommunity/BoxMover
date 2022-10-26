0dc455be8c82a78304f8d1730c8bed67fedbde21
Rom1 <rom1@canel.ch>
2022-10-26 15:59:39 +0200

    doc: Mettre les commentaire sur les fonctions

--- box_mover.c
+++ box_mover.c
@@ -1,73 +1,114 @@
+/*
+ * Project: BoxMover_PoC
+ * File: box_mover.c
+ * Create by: Rom1 <rom1@canel.ch> - Flipper French Community - https://github.com/FlipperFrenchCommunity
+ * Date: 26 octobre 2022
+ * License: GNU GENERAL PUBLIC LICENSE v3
+ * Description: Programme d'apprentissage pour faire une application pour le
+ *              Flipper Zero. Le programme récupère les entrées (boutons) et
+ *              déplace un point sur l'affichage. Apprendre à générer un
+ *              affichage et lire des entrées.
+ */
 #include <furi.h>
 #include <gui/gui.h>
 #include <input/input.h>
 #include <stdlib.h>
 
+
+/*
+ * Structure contenant le positionnement XY du points à déplacer.
+ */
 typedef struct {
     int x;
     int y;
 } BoxMoverModel;
 
+/*
+ * Structure contenant l'état du programme.
+ */
 typedef struct {
-    BoxMoverModel* model;
+    BoxMoverModel* model; // Positionnement du point
 
-    FuriMessageQueue* event_queue;
+    FuriMessageQueue* event_queue; // Queue pour passer des message
 
-    FuriMutex* model_mutex;
+    FuriMutex* model_mutex; // Mutex pour verouille le fil d'exécution (Thread)
 
-    ViewPort* view_port;
-    Gui* gui;
-} BoxMover;
+    ViewPort* view_port; // Structure avec l'état de la vue de l'écran.
+    Gui* gui; // Structure avec l'état de la gestion de l'affichage de l'écran (GUI)
+} BoxMoverState;
 
+/*
+ * Rappel de fonction pour desiner l'écran.
+ */
 void draw_callback(Canvas* canvas, void* ctx){
-    BoxMover* box_mover = ctx;
+    BoxMoverState* box_mover = ctx;
     furi_check(furi_mutex_acquire(box_mover->model_mutex, 25) == FuriStatusOk);
     canvas_draw_box(canvas, box_mover->model->x, box_mover->model->y, 4, 4);
     furi_mutex_release(box_mover->model_mutex);
 }
 
+/*
+ * Rappel de fonction pour lire les entrée
+ */
 void input_callback(InputEvent* input, void* ctx){
-    BoxMover* box_mover = ctx;
+    BoxMoverState* box_mover = ctx;
     furi_check(furi_mutex_acquire(box_mover->model_mutex, 25) == FuriStatusOk);
     furi_message_queue_put(box_mover->event_queue, input, FuriWaitForever);
     furi_mutex_release(box_mover->model_mutex);
 }
 
-BoxMover* box_mover_alloc(){
-    BoxMover* instance = malloc(sizeof(BoxMover));
-    instance->model = malloc(sizeof(BoxMoverModel));
-    instance->model->x = 10;
-    instance->model->y = 10;
+/*
+ * Fonction pour initialiser le programme.
+ */
+BoxMoverState* box_mover_alloc(){
+    BoxMoverState* state = malloc(sizeof(BoxMoverState));
+    state->model = malloc(sizeof(BoxMoverModel));
+    state->model->x = 10;
+    state->model->y = 10;
 
-    instance->view_port = view_port_alloc();
-    instance->model_mutex = furi_mutex_alloc(FuriMutexTypeNormal);
-    instance->gui = furi_record_open(RECORD_GUI);
-    gui_add_view_port(instance->gui, instance->view_port, GuiLayerFullscreen);
+    state->view_port = view_port_alloc();
+    state->model_mutex = furi_mutex_alloc(FuriMutexTypeNormal);
+    state->gui = furi_record_open(RECORD_GUI);
+    gui_add_view_port(state->gui, state->view_port, GuiLayerFullscreen);
 
-    instance->event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+    state->event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
     
-    view_port_draw_callback_set(instance->view_port, draw_callback, instance);
-    view_port_input_callback_set(instance->view_port, input_callback, instance);
+    /* 
+     * Définir les fonctions de rappel pour mettre à jour l'affichage ou la
+     * saisie des entrées
+     */
+    view_port_draw_callback_set(state->view_port, draw_callback, state);
+    view_port_input_callback_set(state->view_port, input_callback, state);
     
-    return instance;
+    return state;
 }
 
-void box_mover_free(BoxMover* instance){
-    view_port_enabled_set(instance->view_port, false);
-    gui_remove_view_port(instance->gui, instance->view_port);
+/*
+ * Fonction pour terminer proprement le programme.
+ */
+void box_mover_free(BoxMoverState* state){
+    view_port_enabled_set(state->view_port, false);
+    gui_remove_view_port(state->gui, state->view_port);
     furi_record_close(RECORD_GUI);
-    view_port_free(instance->view_port);
+    view_port_free(state->view_port);
 
-    furi_message_queue_free(instance->event_queue);
-    furi_mutex_free(instance->model_mutex);
+    furi_message_queue_free(state->event_queue);
+    furi_mutex_free(state->model_mutex);
 
-    free(instance->model);
-    free(instance);
+    free(state->model);
+    free(state);
 }
 
+/*
+ * Point d'entrée principal du programme.
+ */
 int32_t box_mover_app(void* p){
-    UNUSED(p);
-    BoxMover* box_mover = box_mover_alloc();
+    UNUSED(p); // Bloque les erreurs de compilation d'une variable non-initialisée
+
+    /*
+     * Créer l'état de notre programme.
+     */
+    BoxMoverState* box_mover = box_mover_alloc();
 
     InputEvent event;
     for(bool processing=true ; processing ; ){
@@ -99,6 +140,7 @@ int32_t box_mover_app(void* p){
             }
         }
         furi_mutex_release(box_mover->model_mutex);
+        view_port_update(box_mover->view_port);
     }
 
     box_mover_free(box_mover);
