f3feeabb03f611c0606bff2bc1d4826382ecc724
Rom1 <rom1@canel.ch>
2022-10-29 14:01:02 +0200

    Ajoute un commentaire pour chaque instruction

--- box_mover.c
+++ box_mover.c
@@ -31,34 +31,55 @@ typedef struct {
  * Structure contenant l'état du programme.
  */
 typedef struct {
-    BoxMoverModel* model; // Positionnement du pixel
-    Gui* gui; // Gestion de l'affichage de l'écran (GUI)
-    ViewPort* view_port; // Etat de la vue de l'écran.
-    FuriMessageQueue* event_queue; // Fil d'attente (Queue) pour passer des message
-    FuriMutex* model_mutex; // Verrou (Mutex) pour verouiller le fil d'exécution (Thread)
+    BoxMoverModel* model;          // Positionnement du pixel
+    Gui* gui;                      // Gestion de l'affichage de l'écran (GUI)
+    ViewPort* view_port;           // Etat de la vue de l'écran.
+    FuriMessageQueue* event_queue; // Fil d'attente (Queue) pour passer des message (événements en entrée)
+    FuriMutex* model_mutex;        // Verrou (Mutex) pour verouiller le fil d'exécution (Thread)
 } BoxMoverState;
 
-/*
+/**
  * Rappel de fonction pour dessiner l'écran.
+ *
+ * @param       Canvas pour l'affichage de l'écran
+ * @param       Context contenant la variable d'état
  */
 void box_mover_draw_callback(Canvas* canvas, void* context){
+    /* Récupère la variable d'état du programme */
     BoxMoverState* box_mover = context;
+
+    /* Aquérire le verrou  pour bloquer et modifier l'affichage sans risquei de
+     * modification de box_mover->model */
     furi_check(furi_mutex_acquire(box_mover->model_mutex, 25) == FuriStatusOk);
+
+    /* Modifier le canvas d'affichage */
     canvas_draw_box(canvas, box_mover->model->x, box_mover->model->y, 4, 4);
+
+    /* Relacher le verrou */
     furi_mutex_release(box_mover->model_mutex);
 }
 
-/*
+/**
  * Rappel de fonction pour lire les entrée
+ *
+ * @param       Évenement en entrées
+ * @param       Context contenant la variable d'état
  */
 void box_mover_input_callback(InputEvent* input, void* context){
+    /* Récupère la variable d'état du programme */
     BoxMoverState* box_mover = context;
+
+    /* Aquérire le verrou  pour bloquer et modifier l'affichage sans risque */
     furi_check(furi_mutex_acquire(box_mover->model_mutex, 25) == FuriStatusOk);
+
+    /* Met les événements en entrée dans la fil d'attente */
     furi_message_queue_put(box_mover->event_queue, input, FuriWaitForever);
+
+    /* Relacher le verrou */
     furi_mutex_release(box_mover->model_mutex);
 }
 
-/*
+/**
  * Fonction pour initialiser le programme. Elle va allouer les différentes ressources 
  * du programme dans la structure (BoxMoverState) contenant son état.
  *
@@ -83,22 +104,20 @@ BoxMoverState* box_mover_alloc(){
     /* Alloue une fil d'attente pour les événements en entrée */
     state->event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
     
-    /* 
-     * Définir les fonctions de rappel pour mettre à jour l'affichage ou la
-     * saisie des entrées
-     */
+    /* Définir les fonctions de rappel pour mettre à jour l'affichage ou la
+     * saisie des entrées */
     view_port_draw_callback_set(state->view_port, box_mover_draw_callback, state);
     view_port_input_callback_set(state->view_port, box_mover_input_callback, state);
     
     return state; // Retourn la structure d'état
 }
 
-/*
+/**
  * Fonction pour terminer proprement le programme. Elle va libérer les ressource
  * avant de terminer le programme.
  *
  * @param       Structure contenant l'état du programme
- */:
+ */
 void box_mover_free(BoxMoverState* state){
     view_port_enabled_set(state->view_port, false);
     gui_remove_view_port(state->gui, state->view_port);
@@ -112,53 +131,63 @@ void box_mover_free(BoxMoverState* state){
     free(state);
 }
 
-/*
+/**
  * Point d'entrée principal du programme.
  */
 int32_t box_mover_app(void* p){
     UNUSED(p); // Bloque les erreurs de compilation d'une variable non-initialisée
 
-    /*
-     * Créer l'état de notre programme.
-     */
-    BoxMoverState* box_mover = box_mover_alloc();
+    BoxMoverState* box_mover = box_mover_alloc(); // Créer l'état de notre programme
 
-    /*
-     * Boucle d'événement qui recherche si il y a une entrée.
-     */
-    InputEvent event;
+
+    InputEvent event; // Variable contenant les événements en entrée
+    
+    /* Boucle d'événement qui lit si une entrée saisie. La boucle est réalisée tant
+     * que la touche retour en arrière n'est pas pressée. */
     for(bool processing=true ; processing ; ){
+
+        /* Récupère les événement d'entrée mis dans la fil d'attente */
         FuriStatus event_status = furi_message_queue_get(box_mover->event_queue,
         &event, 100);
 
+        /* Prend le verrou */
         furi_check(furi_mutex_acquire(box_mover->model_mutex, 25) == FuriStatusOk);
 
+        /* Si il y a un événement faire */
         if(event_status == FuriStatusOk) {
+            /* Si une entrée est pressée faire */
             if(event.type == InputTypePress) {
+                /* Aiguille en fonction des divers entrées pressées */
                 switch(event.key) {
+                    /* Bouton du haut */
                     case InputKeyUp:
-                        box_mover->model->y -= 2;
+                        box_mover->model->y -= 2; // Déplace le pixel sur le haut
                         break;
+                    /* Bouton du bas */
                     case InputKeyDown:
-                        box_mover->model->y += 2;
+                        box_mover->model->y += 2; // Déplace le pixel sur le bas
                         break;
+                    /* Bouton de gauche */
                     case InputKeyLeft:
-                        box_mover->model->x -= 2;
+                        box_mover->model->x -= 2; // Déplace le pixel sur la gauche
                         break;
+                    /* Bouton de droite */
                     case InputKeyRight:
-                        box_mover->model->x += 2;
+                        box_mover->model->x += 2; // Déplace le pixel sur la droite
                         break;
+
+                    /* Bouton du centre et de retour en arrière */
                     case InputKeyOk:
                     case InputKeyBack:
-                        processing = false;
+                        processing = false; // Stop la boucle
                         break;
                 }
             }
         }
-        furi_mutex_release(box_mover->model_mutex);
-        view_port_update(box_mover->view_port);
+        furi_mutex_release(box_mover->model_mutex); // Relache le verrou
+        view_port_update(box_mover->view_port); // Met à jour l'affichage 
     }
 
-    box_mover_free(box_mover);
+    box_mover_free(box_mover); // Libère l'état de notre programme
     return 0;
 }
